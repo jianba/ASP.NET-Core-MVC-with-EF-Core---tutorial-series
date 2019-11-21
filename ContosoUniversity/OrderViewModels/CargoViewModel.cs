2d011978787a5db3c8a71421c85581ffea15916c
jianba <szy@aiursoft.com>
2019-11-21 19:53:19 +0800

    Init Order module

---
+++ ContosoUniversity/OrderViewModels/CargoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.OrderViewModels
+{
+    public class CargoViewModel
+    {
+        public int Amount { get; set; }
+        public string CargoType { get; set; }
+    }
+}
---
+++ ContosoUniversity/OrderViewModels/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.OrderViewModels
+{
+    public class OrderViewModel
+    {
+        public string PickUpAddress { get; set; }
+        public string DropOffAddress { get; set; }
+        public List<Cargo> Cargos { get; set; }
+    }
+}
