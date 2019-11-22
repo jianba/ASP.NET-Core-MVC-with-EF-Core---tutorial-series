7792136a013f8ce2c40b3e95a57b00738d9f81d0
jianba <szy@aiursoft.com>
2019-11-22 20:42:53 +0800

    Implement Order index page.

--- ContosoUniversity/Controllers/OrderController.cs
+++ ContosoUniversity/Controllers/OrderController.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.OrderViewModels;
+using ContosoUniversity.OrderViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Index()
+        private readonly SchoolContext _context;
+
+        public OrderController(SchoolContext context)
         {
-            return View();
+            _context = context;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var ordersViewModel = new OrdersViewModel();
+            var orders = await _context.Order.Select(o => o).ToListAsync();
+
+            foreach (var orderItem in orders)
+            {
+                var tmpOrderViewModel = new OrderViewModel
+                {
+                    OrderId = orderItem.OrderId,
+                    PickUpAddress = orderItem.PickUpAddress,
+                    DropOffAddress = orderItem.DropOffAddress
+                };
+                var cargos = await _context.Cargo
+                    .Where(o => o.OrderId == orderItem.OrderId)
+                    .Select(u =>
+                    new CargoViewModel
+                    {
+                        OrderId = u.OrderId,
+                        Amount = u.Amount,
+                        CargoType = u.CargoType
+                    }).ToListAsync();
+
+                tmpOrderViewModel.Cargos = cargos;
+
+                ordersViewModel.Orders.Add(tmpOrderViewModel);
+            }
+
+            return View(ordersViewModel);
+        }
+
     }
 }
--- ContosoUniversity/Models/OrderViewModels/OrderViewModel.cs
+++ ContosoUniversity/Models/OrderViewModels/OrderViewModel.cs
@@ -8,8 +8,9 @@ namespace ContosoUniversity.OrderViewModels
 {
     public class OrderViewModel
     {
+        public int OrderId { get; set; }
         public string PickUpAddress { get; set; }
         public string DropOffAddress { get; set; }
-        public List<Cargo> Cargos { get; set; }
+        public List<CargoViewModel> Cargos { get; set; }
     }
 }
