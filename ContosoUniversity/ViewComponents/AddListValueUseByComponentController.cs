c5077430bfdcf8a80a1725956e19ff6d42e44ad3
c <15657868169m@sina.cn>
2019-12-08 01:06:27 +0800

    Dynamic insert drop-down box

---
+++ ContosoUniversity/ViewComponents/AddListValueUseByComponentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoUniversity.ViewComponents
+{
+    public class AddListValueUseByComponentController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public AddListValueUseByComponentController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SaveInputValue(string name)
+        {
+            var test = new Test
+            {
+                Name = name
+            };
+
+            await _context.AddAsync(test);
+            await _context.SaveChangesAsync();
+
+            return Json(new
+            {
+                code = 1
+            });
+        }
+    }
+}
---
+++ ContosoUniversity/ViewComponents/AutoAddListValueViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ContosoUniversity.Models.TestViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.ViewComponents
+{
+    public class AutoAddListValueViewComponent : ViewComponent
+    {
+        private readonly SchoolContext _context;
+
+        public AutoAddListValueViewComponent(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+            int maxPriority, bool isDone)
+        {
+            string MyView = "Default";
+
+            var items = await GetListAsync();
+            return View(MyView, items);
+        }
+
+        public async Task<List<string>> GetListAsync()
+        {
+            return await _context.Test.Select(o => o.Name).ToListAsync();
+        }
+    }
+}
